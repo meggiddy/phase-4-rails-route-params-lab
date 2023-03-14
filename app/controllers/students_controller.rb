d23a1935ea76d8a132c2caf89d63ddd4cd7b6659
meggidy <mimamitchelle@gmail.com>
2023-03-14 12:16:03 +0300

    completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:query]
+      students = Student.where("lower(first_name) LIKE ? OR lower(last_name) LIKE ?", "%#{params[:query].downcase}%", "%#{params[:query].downcase}%")
+    elsif params[:name]
+      students = Student.where("lower(first_name) LIKE ? OR lower(last_name) LIKE ?", "%#{params[:name].downcase}%", "%#{params[:name].downcase}%")
+    else
+      students = Student.all
+    end
+    render json: @students || []
+  end
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
